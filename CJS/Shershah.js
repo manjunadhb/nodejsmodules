3a7ab0048898e6bba3d94035a95f49499a7db209
manjunadhb <manjunadhb@gmail.com>
2024-05-23 17:36:15 +0530

    Add files via upload

---
+++ CJS/Shershah.js
@@ -0,0 +1,8 @@
+let movieTitle = "Shershah";
+let movieActors = ["Siddarth Malhotra", "Kiara Advani"];
+
+let aboutShershahMovie = () => {
+  console.log("Story about captain vikram batra.");
+};
+
+module.exports = { movieTitle, movieActors, aboutShershahMovie };
