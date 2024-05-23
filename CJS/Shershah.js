a7301f1df84e6fe7e10899e7c62af73802c977a2
manjunadhb <manjunadhb@gmail.com>
2024-05-23 17:35:42 +0530

    Delete CJS directory

--- CJS/Shershah.js
+++
@@ -1,8 +0,0 @@
-let movieTitle = "Shershah";
-let movieActors = ["Siddarth Malhotra", "Kiara Advani"];
-
-let aboutShershahMovie = () => {
-  console.log("Story about captain vikram batra.");
-};
-
-module.exports = { movieTitle, movieActors, aboutShershahMovie };
--- CJS/jailer.js
+++
@@ -1,8 +0,0 @@
-let movieTitle = "Jailer";
-let actors = ["Rajnikanth", "Mohan Lal", "Shivraj Kumar", "Ramya Krishna"];
-
-let aboutJailerStory = () => {
-  console.log("Story about a retired jailer.");
-};
-
-module.exports = { movieTitle, actors, aboutJailerStory };
